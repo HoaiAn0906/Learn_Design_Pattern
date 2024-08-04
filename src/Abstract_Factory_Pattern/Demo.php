e0f466e0059b14d221013c42e0521b4ee0867540
An Phan Hoài <phoaianvlogs@gmail.com>
2024-08-04 12:47:48 +0700

    Feat: Abstract Factory Pattern

--- src/Abstract_Factory_Pattern/Demo.php
+++ src/Abstract_Factory_Pattern/Demo.php
@@ -7,6 +7,18 @@
     use App\src\Abstract_Factory_Pattern\factories\LinuxFactory;
     use App\src\Abstract_Factory_Pattern\factories\WindowFactory;
 
+    /*
+    Abstract Factory Pattern (Creational Design Patterns)
+    Problem: You need to create a family of related objects, but you don't want to depend on their concrete classes.
+    Solution: Define an interface for creating an object, but let subclasses decide which class to instantiate. Factory Method lets a class defer instantiation to subclasses.
+    Structure of Abstract Factory Pattern
+        AbstractFactory: Declare an interface for operations to create abstract product objects. (GUIFactory)
+        ConcreteFactory: Implement operations to create specific product objects. (WindowFactory, LinuxFactory)
+        AbstractProduct: Declare an interface for a product object type. (Button, Checkbox)
+        Product: Implement interface of AbstractProduct. (WindowButton, LinuxButton, WindowCheckbox, LinuxCheckbox)
+        Client: Use the AbstractFactory and AbstractProduct interfaces to work with concrete objects. (Application)
+    */
+    
     class Demo
     {
         public static function run(): string
